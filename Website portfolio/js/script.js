5ffc02f8292606366ca0caafdabed72f9b7500e2
Alexander Wake <alec.wake@icloud.com>
2025-03-25 13:02:43 -0500

    initial commit

---
+++ Website portfolio/js/script.js
@@ -0,0 +1,17 @@
+document.getElementById('read-more-btn').addEventListener('click', function(){
+    const shortBio = document.getElementById('short-bio');
+    const longBio = document.getElementById('long-bio');
+    const btn = document.getElementById('read-more-btn');
+
+    // Toggle visibility of short and long bio
+    if(shortBio.style.display === "none"){
+        shortBio.style.display = "block";
+        longBio.style.display = "none";
+        btn.innerText = "Read More";
+    }
+    else{
+        shortBio.style.display = "none";
+        longBio.style.display = "block";
+        btn.innerText = "Show Less";
+    }
+});
